8ca0086451273af0f55ead1d8c8aba570f155db1
hdnguyendev <nguyenhd.dev@gmail.com>
2024-05-06 23:28:37 +0700

    feature: character select scene

--- Assets/Scripts/CharacterSelectReady.cs
+++ Assets/Scripts/CharacterSelectReady.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -6,6 +7,8 @@ using UnityEngine;
 public class CharacterSelectReady : NetworkBehaviour
 {
     public static CharacterSelectReady Instance { get; private set; }
+
+    public event EventHandler OnReadyChanged;
     private Dictionary<ulong, bool> playerReadyDictionary;
 
     private void Awake()
@@ -14,24 +17,43 @@ public class CharacterSelectReady : NetworkBehaviour
 
         playerReadyDictionary = new Dictionary<ulong, bool>();
     }
-    public void SetPlayerReady() {
+    public void SetPlayerReady()
+    {
         SetPlayerReadyServerRpc();
     }
     [ServerRpc(RequireOwnership = false)]
-    private void SetPlayerReadyServerRpc(ServerRpcParams serverRpcParams = default) {
+    private void SetPlayerReadyServerRpc(ServerRpcParams serverRpcParams = default)
+    {
+        SetPlayerReadyClientRpc(serverRpcParams.Receive.SenderClientId);
+
         playerReadyDictionary[serverRpcParams.Receive.SenderClientId] = true;
 
         bool allClientsReady = true;
-        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds) {
-            if (!playerReadyDictionary.ContainsKey(clientId) || !playerReadyDictionary[clientId]) {
+        foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
+        {
+            if (!playerReadyDictionary.ContainsKey(clientId) || !playerReadyDictionary[clientId])
+            {
                 // This player is NOT ready
                 allClientsReady = false;
                 break;
             }
         }
-        if (allClientsReady) {
+        if (allClientsReady)
+        {
             Loader.LoadNetwork(Loader.Scene.GameScene);
         }
 
+    }
+    [ClientRpc]
+    private void SetPlayerReadyClientRpc(ulong clientId)
+    {
+        playerReadyDictionary[clientId] = true;
+        OnReadyChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+
+    public bool IsPlayerReady(ulong clientId)
+    {
+        return playerReadyDictionary.ContainsKey(clientId) && playerReadyDictionary[clientId];
     }
 }
--- Assets/Scripts/UI/HostDisconnectUI.cs
+++ Assets/Scripts/UI/HostDisconnectUI.cs
@@ -37,4 +37,8 @@ public class HostDisconnectUI : MonoBehaviour
     {
         gameObject.SetActive(false);
     }
+
+    private void OnDestroy() {
+        NetworkManager.Singleton.OnClientDisconnectCallback -= NetworkManager_OnClientDisconnectCallback;
+    }
 }
